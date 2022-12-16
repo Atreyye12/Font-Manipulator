f842f6254f707037388f441f69130c14016b9af7
Atreyye12 <102455332+Atreyye12@users.noreply.github.com>
2022-12-16 15:42:52 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -13,12 +13,29 @@ function modelLoaded(){
     console.log('Posenet Is Initialized!')
 }
 
-function draw(){
-    background('#00FFFF')
-}
-
-function gotPoses(results){
-    if(results.length > 0){
-        console.log(results);
+function draw() {
+    background('#969A97');
+    
+      document.getElementById("square_side").innerHTML = "Width And Height of a Square will be = " + difference +"px";
+      Text("respect is for those who deseve it not for those who demand it")
+      fill('#F90093');
+      stroke('#F90093');
+      square(noseX, noseY, difference);
     }
-}
+    function gotPoses(results)
+    {
+      if(results.length > 0)
+      {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX +" noseY = " + noseY);
+    
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+    
+        console.log("leftWristX  = " + leftWristX  + " rightWristX = "+ rightWristX + " difference = " + difference);
+      }
+    }    
+
